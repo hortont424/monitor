1eec5fdc69e6d492e1cadd4ab5ccc1a2f499b7aa
Tim Horton <hortont424@gmail.com>
2011-01-03 14:11:48 -0500

    We can't really generalize this now that Matt wants to look at the validator status page and watch for jumps and stuff

--- monitor.py
+++ monitor.py
@@ -5,6 +5,8 @@ import datetime
 import os.path
 import platform
 
+HOSTNAME = "milkyway.cs.rpi.edu"
+
 def ping(hostname):
     if platform.system() == "Darwin" or platform.system() == "BSD":
         cmd = ["ping", "-t", "5", "-o", hostname]
@@ -14,17 +16,6 @@ def ping(hostname):
     proc = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
     return proc.wait() == 0
 
-def load_config(filename):
-    file = open(filename, "r")
-
-    if not file:
-        print "Failed to open configuration file '{0}'...".format(filename)
-
-    config = json.load(file)
-    file.close()
-
-    return config
-
 def write_config(filename, config):
     file = open(filename, "w+")
     file.write(json.dumps(config, sort_keys=True, indent=4))
@@ -32,12 +23,9 @@ def write_config(filename, config):
 
 def __main__():
     dir = os.path.dirname(__file__)
-    config = load_config(os.path.join(dir, "config.json"))
+    config = {}
 
-    for (name, project) in config["projects"].items():
-        for (index, (hostname, title)) in enumerate(project["ping_hosts"]):
-            project["ping_hosts"][index].append(ping(hostname))
-
+    config["ping"] = ping(HOSTNAME)
     config["date"] = datetime.datetime.utcnow().strftime("%Y.%m.%d %H:%M:%S")
 
     write_config(os.path.join(dir, "output.json"), config)
